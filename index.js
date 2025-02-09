6508a17a03628cd91738e6198aa3b2fb5442ef59
Thomas Carbonell <tcarbonell12@gmail.com>
2025-02-08 20:06:24 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove('main');
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Thomas is the champion";
+
+
+document.body.append(newHeader);
